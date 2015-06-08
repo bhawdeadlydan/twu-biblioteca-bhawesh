7fcb42ec65046446a91109f20030fde8632356d3
Bhawesh Kumar Singh <bhawdeadlydan@gmail.com>
2015-06-08 06:35:51 +0530

    Bhawesh - Added Spec for checking Whether Wlecome message Is being displayed or not

---
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -0,0 +1,7 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by bhawesh on 08/06/15.
+ */
+public class BibliotecaApp {
+}
---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,10 @@
+package com.twu.biblioteca;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by bhawesh on 08/06/15.
+ */
+public class BibliotecaAppTest extends TestCase {
+
+}
