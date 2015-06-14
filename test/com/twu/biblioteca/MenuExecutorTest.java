54e53b6ccd8be81d428eea4b84cbceb5de37cf01
Bhawesh Kumar Singh <bhawdeadlydan@gmail.com>
2015-06-15 00:29:10 +0530

    Spec for prompting user to select a valid option on invalid menu option entry

--- test/com/twu/biblioteca/MenuExecutorTest.java
+++ test/com/twu/biblioteca/MenuExecutorTest.java
@@ -13,39 +13,44 @@ import static org.mockito.Mockito.when;
 
 public class MenuExecutorTest {
 
-        @Mock
-        ConsoleView consoleViewStub1;
+    @Mock
+    ConsoleView consoleViewStub1;
 
-        @Mock
-        ConsoleView consoleViewStub2;
-
-
-        private MenuExecutor menuExecutor;
-
-        private HashMap<Integer, MenuAction> menuItemMap;
-
-        @Before
-        public void setUp() {
-            MockitoAnnotations.initMocks(this);
-
-            ArrayList<String[]> bookList = new ArrayList<String[]>();
-            bookList.add(new String[]{"Book 1", "JK Rowling", "2003"});
-            bookList.add(new String[]{"Book 2", "Arthur Conan Doyle", "1886"});
-            bookList.add(new String[]{"Book 3", "Agatha Christie", "1800"});
-
-            Books books = new Books(bookList);
-            menuItemMap = new HashMap<Integer, MenuAction>();
-            menuItemMap.put(1, new ListBooks(books, consoleViewStub1));
-            menuExecutor = new MenuExecutor(menuItemMap, consoleViewStub2);
-        }
-
-        @Test
-        public void shouldTakeUserChoice() {
-            menuExecutor.executeUserCommand();
-
-            verify(consoleViewStub2).read();
-        }
+    @Mock
+    ConsoleView consoleViewStub2;
 
 
+    private MenuExecutor menuExecutor;
 
+    private HashMap<Integer, MenuAction> menuItemMap;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        ArrayList<String[]> bookList = new ArrayList<String[]>();
+        bookList.add(new String[]{"Book 1", "JK Rowling", "2003"});
+        bookList.add(new String[]{"Book 2", "Arthur Conan Doyle", "1886"});
+        bookList.add(new String[]{"Book 3", "Agatha Christie", "1800"});
+
+        Books books = new Books(bookList);
+        menuItemMap = new HashMap<Integer, MenuAction>();
+        menuItemMap.put(1, new ListBooks(books, consoleViewStub1));
+        menuExecutor = new MenuExecutor(menuItemMap, consoleViewStub2);
     }
+
+    @Test
+    public void shouldTakeUserChoice() {
+        menuExecutor.executeUserCommand();
+
+        verify(consoleViewStub2).read();
+    }
+
+    @Test
+    public void shouldDisplayListOfBooksIfUserInputIsOne() {
+        when(consoleViewStub2.read()).thenReturn(1);
+        menuExecutor.executeUserCommand();
+
+        verify(consoleViewStub1).print("\nName\tAuthor\tPublication Year\nBook 1\tJK Rowling\t2003\nBook 2\tArthur Conan Doyle\t1886\nBook 3\tAgatha Christie\t1800");
+    }
+}
