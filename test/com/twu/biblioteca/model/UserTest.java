9dd26b9c49229e30af497d02e529cc1f1f0987a7
Bhawesh Kumar Singh <bhawdeadlydan@gmail.com>
2015-06-22 08:54:33 +0530

    Spec for equals and hashcode using template for user class

--- test/com/twu/biblioteca/model/UserTest.java
+++ test/com/twu/biblioteca/model/UserTest.java
@@ -14,4 +14,46 @@ public class UserTest {
         String expectedOutput = "userName='888-8888', name='Rajeev', password='rogerpass', phone='9898000098', email='raj@gmail.com'";
         assertThat(actualOutput, is(expectedOutput));
     }
+
+    @Test
+    public void shouldBeEqualToItself() {
+        User firstUser = new User("Rajeev","888-8888","rogerpass", "raj@gmail.com", "9898000098");
+
+        boolean actual = firstUser.equals(firstUser);
+
+        assertThat(actual, is(true));
+    }
+
+    @Test
+    public void shouldFollowSymmetricProperty() {
+        User firstUser = new User("Rajeev","888-8888","rogerpass", "raj@gmail.com", "9898000098");
+        User secondUser = new User("Rajeev","888-8888","rogerpass", "raj@gmail.com", "9898000098");
+
+        boolean actual = (firstUser.equals(secondUser) == secondUser.equals(firstUser));
+
+        assertThat(actual, is(true));
+    }
+
+    @Test
+    public void shouldFollowTransitiveProperty() {
+        User firstUser = new User("Rajeev","888-8888","rogerpass", "raj@gmail.com", "9898000098");
+        User secondUser = new User("Rajeev","888-8888","rogerpass", "raj@gmail.com", "9898000098");
+        User thirdUser = new User("Rajeev","888-8888","rogerpass", "raj@gmail.com", "9898000098");
+
+        boolean actual = firstUser.equals(secondUser) &&
+                secondUser.equals(thirdUser) &&
+                firstUser.equals(thirdUser);
+
+        assertThat(actual, is(true));
+    }
+
+    @Test
+    public void shouldHaveSameHashCodeIfUsersAreEqual() {
+        User firstUser = new User("Rajeev","888-8888","rogerpass", "raj@gmail.com", "9898000098");
+        User secondUser = new User("Rajeev","888-8888","rogerpass", "raj@gmail.com", "9898000098");
+
+        boolean actual = firstUser.equals(secondUser) && (firstUser.hashCode() == secondUser.hashCode());
+
+        assertThat(actual, is(true));
+    }
 }
